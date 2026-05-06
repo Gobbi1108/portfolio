4e75ba815b13fc4f7bea34c070403a0eb52434d1
Gabriel Gobbi <gabgobs@gmail.com>
2026-05-06 20:22:45 -0300

    chore: initial project setup

---
+++ eslint.config.cjs
@@ -0,0 +1,51 @@
+// eslint.config.cjs
+const js = require('@eslint/js');
+const tsParser = require('@typescript-eslint/parser');
+const tsPlugin = require('@typescript-eslint/eslint-plugin');
+const astro = require('eslint-plugin-astro');
+const jsxA11y = require('eslint-plugin-jsx-a11y');
+const astroParser = require('astro-eslint-parser');
+
+module.exports = [
+  {
+    ignores: [
+      '.astro/**',
+      'dist/**',
+      'build/**',
+      '.vercel/**',
+      '.netlify/**',
+    ],
+  },
+
+  js.configs.recommended,
+  ...astro.configs['flat/recommended'],
+  jsxA11y.flatConfigs.recommended,
+
+  {
+    files: ['**/*.{ts,tsx,mts,cts}'],
+    languageOptions: {
+      parser: tsParser,
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+    },
+    rules: {
+      ...tsPlugin.configs.recommended.rules,
+      '@typescript-eslint/no-explicit-any': 'error',
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    },
+  },
+
+  {
+    files: ['**/*.astro'],
+    languageOptions: {
+      parser: astroParser,
+      parserOptions: {
+        parser: tsParser,
+        extraFileExtensions: ['.astro'],
+      },
+    },
+  },
+];
